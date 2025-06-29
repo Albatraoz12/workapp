1e535f98f303446d2d309b6f69fdb2b5cec8dc6b
Dimosthenis Emmanouil <dimothenis.emmanouil@gmail.com>
2025-06-29 21:15:40 +0200

    create a dashboard component with design

---
+++ app/components/UserInfo.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const UserInfo = () => {
+  return (
+    <div className="grid place-items-center h-sscreen">
+      <div className="shadow-lg p-8 bg-zinc-300/10 flex flex-col gap-2 my-6">
+        <div>
+          Name: <span className="font-bold">Dimosthenis</span>
+        </div>
+        <div>
+          Email: <span className="font-bold">d123@gmail.com</span>
+        </div>
+        <div>
+          Work Place: <span className="font-bold">Karolinska</span>
+        </div>
+        <button className="bg-red-500 text-white font-bold px-6 py-2 mt-3 cursor-pointer">
+          Log Out
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default UserInfo;
