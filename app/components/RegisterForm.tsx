f24975f333a8201947662f8ea101a42dfcc599ff
Dimosthenis Emmanouil <dimothenis.emmanouil@gmail.com>
2025-06-29 21:24:31 +0200

    create states to store values to be sent to api

--- app/components/RegisterForm.tsx
+++ app/components/RegisterForm.tsx
@@ -1,23 +1,57 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const RegisterForm = () => {
+  const [name, setName] = useState("");
+  const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [workPlace, setWorkPlace] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!name || !email || !password || !workPlace) {
+      setError("All feilds are necesary!");
+      return;
+    }
+  };
+
   return (
     <div className="grid place-items-center h-screen">
       <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
         <h1 className="text-xl font-bold my-4">Register</h1>
-        <form className="flex flex-col gap-3">
-          <input type="text" placeholder="Full name" />
-          <input type="text" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="text" placeholder="Work place" />
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+          <input
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Full name"
+          />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+          />
+          <input
+            onChange={(e) => setWorkPlace(e.target.value)}
+            type="text"
+            placeholder="Work place"
+          />
           <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
             Register
           </button>
 
-          <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-            Error message
-          </div>
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
 
           <Link className="text-sm mt-3 text-right" href="/">
             Already have an account? <span className="underline">Log In</span>
