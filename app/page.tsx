40ba08e4b5c9c7a45ca408c52c96e45058e278c8
Dimosthenis Emmanouil <dimothenis.emmanouil@gmail.com>
2025-06-29 21:05:16 +0200

    create loginform design

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import LoginForm from "./components/LoginForm";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <main className="h-[100vh]">
+      <LoginForm />
+    </main>
   );
 }
