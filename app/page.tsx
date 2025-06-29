96935c718e4b215526a01e46ef7dc93192f693ce
Dimosthenis Emmanouil <dimothenis.emmanouil@gmail.com>
2025-06-29 22:58:32 +0200

    redirect users to dashboard when logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import LoginForm from "./components/LoginForm";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  session && redirect("/dashboard");
+
   return (
     <main className="h-[100vh]">
       <LoginForm />
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import RegisterForm from "../components/RegisterForm";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
-const page = () => {
+const page = async () => {
+  const session = await getServerSession(authOptions);
+
+  session && redirect("/dashboard");
+
   return <RegisterForm />;
 };
 
