bd844445cce3ddea0ee8352d8dcf44c85804d669
Dimosthenis Emmanouil <dimothenis.emmanouil@gmail.com>
2025-07-06 01:05:42 +0200

    add validation to api to only let logged in users create an item

--- app/api/addItem/route.ts
+++ app/api/addItem/route.ts
@@ -1,12 +1,20 @@
 import { connectMongoDB } from "@/lib/mongodb";
 import Item from "@/models/item";
 import { NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
 
 export async function POST(req: Request) {
   try {
+    const session = await getServerSession(authOptions);
+
+    if (!session)
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+
+    console.log(session);
+
     const { productName, supplierName, orderWhen, quantity, storedLocation } =
       await req.json();
-    console.log(productName, supplierName, orderWhen, quantity, storedLocation);
     await connectMongoDB();
     await Item.create({
       productName,
